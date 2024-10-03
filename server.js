222307c49342d2c353936600a81733257320b2d9
Bruno Souza <souza.brunoha@gmail.com>
2024-10-03 00:38:43 -0300

    home do admin

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const livereload = require('livereload');
+const connectLiveReload = require('connect-livereload');
+
+const app = express();
+
+// Configuração do livereload
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+
+liveReloadServer.server.once('connection', () => {
+    setTimeout(() => {
+        liveReloadServer.refresh('/');
+    }, 100);
+});
+
+app.use(connectLiveReload());
+
+// Servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'admin', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
